b6f337bfeeb1595042b2a2efe3435b7f271e3459
Chris Blyth <blythmeister@gmail.com>
2024-12-19 12:28:21 +0000

    Update sensor.py

--- custom_components/hildebrandglow_dcc/sensor.py
+++ custom_components/hildebrandglow_dcc/sensor.py
@@ -262,7 +262,7 @@ class Usage(SensorEntity):
     _attr_has_entity_name = True
     _attr_name = "Usage (today)"
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _attr_state_class = SensorStateClass.TOTAL
 
     def __init__(self, hass: HomeAssistant, resource, virtual_entity) -> None:
         """Initialize the sensor."""
